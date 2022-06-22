b977ef84bb0e30404eebcc7eb152879531e64310
BSCS-S19-ID-16608 <BSCS-S19-ID-16608@student.numl.edu.pk>
2022-06-23 00:53:49 +0500

    Add project files.

---
+++ Linq-To-Json/ProductForm.aspx
@@ -0,0 +1,96 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ProductForm.aspx.cs" Inherits="Linq_To_Json.ProductForm" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            width: 37px;
+        }
+        .auto-style2 {
+            margin-left: 46px;
+        }
+        .auto-style3 {
+            margin-left: 47px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <table>
+            <tr>
+                <td class="auto-style1">
+                    &nbsp;<td>
+                    <asp:Label ID="Label2" runat="server" Text="ID"></asp:Label>
+                    <asp:TextBox ID="txtId" runat="server"></asp:TextBox>
+                </td>
+            </tr>
+
+            <tr>
+                <td class="auto-style1">
+                    &nbsp;</td>
+                <td>
+                    <asp:Label ID="Label7" runat="server" Text="Title"></asp:Label>
+                    <asp:TextBox ID="txtTitle" runat="server"></asp:TextBox>
+                </td>
+            </tr>
+
+            <tr>
+                <td class="auto-style1">
+                    &nbsp;</td>
+                <td>
+                    <asp:Label ID="Label6" runat="server" Text="Price"></asp:Label>
+                    <asp:TextBox ID="txtPrice" runat="server"></asp:TextBox>
+                </td>
+            </tr>
+
+            <tr>
+                <td class="auto-style1">
+                    &nbsp;</td>
+                <td>
+                    <asp:Label ID="Label5" runat="server" Text="Expire"></asp:Label>
+                    <asp:TextBox ID="txtExpiry" runat="server"></asp:TextBox>
+                </td>
+            </tr>
+
+            <tr>
+                <td class="auto-style1">
+                    &nbsp;</td>
+                <td>
+                    <asp:Label ID="Label4" runat="server" Text="Manufacturer"></asp:Label>
+                    <asp:TextBox ID="txtManufacturer" runat="server"></asp:TextBox>
+                </td>
+            </tr>
+
+            <tr>
+                <td class="auto-style1">
+                    &nbsp;</td>
+                <td>
+                    <asp:Label ID="Label3" runat="server" Text="Stock"></asp:Label>
+                    <asp:TextBox ID="txtStock" runat="server"></asp:TextBox>
+                </td>
+            </tr>
+
+            <tr>
+                <td class="auto-style1">
+                    &nbsp;</td>
+                <td>
+                    <asp:Button ID="btnInsert" runat="server" Text="Insert" OnClick="btnInsert_Click"></asp:Button>
+                    <asp:Button ID="BtnUpdate" runat="server" Text="Update" OnClick="btnUpdate_Click"></asp:Button>
+                    <asp:Button ID="BtnDelete" runat="server" Text="Delete" OnClick="btnDelete_Click"></asp:Button>
+                    <asp:Button ID="BtnSearch" runat="server" Text="Search" OnClick="btnSearch_Click"></asp:Button>
+                </td>
+            </tr>
+
+        </table>
+
+        <asp:TextBox runat="server" ID="txtMain" TextMode="MultiLine" Rows="10" Height="143px" Width="729px" CssClass="auto-style2"></asp:TextBox><br />
+        <asp:Button ID="btnSaveToFile" runat="server" Text="Save To File" OnClick="btnSaveToFile_Click" CssClass="auto-style3"></asp:Button>
+        <br /><br /><br />
+        <asp:GridView ID="gridView" runat="server" Width="736px" CssClass="auto-style2">
+        </asp:GridView>
+    </form>
+</body>
+</html>
---
+++ Linq-To-Json/ProductForm.aspx.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Script.Serialization;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Newtonsoft.Json;
+
+namespace Linq_To_Json
+{
+    public partial class ProductForm : System.Web.UI.Page
+    {
+        List<Dictionary<string, object>> rows;
+        JavaScriptSerializer serializer = new JavaScriptSerializer();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            StreamReader reader = new StreamReader(Server.MapPath("Products.json"));
+            string json = reader.ReadToEnd();
+            rows = serializer.Deserialize<List<Dictionary<string, object>>>(json);
+            reader.Close();
+            if (!IsPostBack)
+            {
+                txtMain.Text = serializer.Serialize(rows);
+            }
+            gridView.DataSource = DisplayJsonData();
+            gridView.DataBind();
+        }
+
+        protected void btnInsert_Click(object sender, EventArgs e)
+        {
+            Dictionary<string, object> row = new Dictionary<string, object>();
+            row.Add("P_ID", txtId.Text);
+            row.Add("P_Title", txtTitle.Text);
+            row.Add("P_Price", txtPrice.Text);
+            row.Add("P_Expiry", txtExpiry.Text);
+            row.Add("P_Manufacturer", txtManufacturer.Text);
+            row.Add("P_Stock", txtStock.Text);
+            rows.Add(row);
+            txtMain.Text = serializer.Serialize(rows);
+        }
+
+        protected void btnUpdate_Click(object sender, EventArgs e)
+        {
+            Dictionary<string, object> row = new Dictionary<string, object>();
+            row.Add("P_ID", txtId.Text);
+            row = rows.SingleOrDefault(x => row.All(x.Contains));
+            row["P_Title"] = txtTitle.Text;
+            row["P_Price"] = txtPrice.Text;
+            row["P_Expiry"] = txtExpiry.Text;
+            row["P_Manufacturer"] = txtManufacturer.Text;
+            row["P_Stock"] = txtStock.Text;
+            txtMain.Text = serializer.Serialize(rows);
+        }
+
+        protected void btnDelete_Click(object sender, EventArgs e)
+        {
+            Dictionary<string, object> row = new Dictionary<string, object>();
+            row.Add("P_ID", txtId.Text);
+            row = rows.SingleOrDefault(x => row.All(x.Contains));
+            rows.Remove(row);
+            txtMain.Text = serializer.Serialize(rows);
+        }
+
+        protected void btnSearch_Click(object sender, EventArgs e)
+        {
+            Dictionary<string, object> row = new Dictionary<string, object>();
+            row.Add("P_ID", txtId.Text);
+            row = rows.SingleOrDefault(x => row.All(x.Contains));
+            txtTitle.Text = row["P_Title"].ToString();
+            txtPrice.Text = row["P_Price"].ToString();
+            txtExpiry.Text = row["P_Expiry"].ToString();
+            txtManufacturer.Text = row["P_Manufacturer"].ToString();
+            txtStock.Text = row["P_Stock"].ToString();
+        }
+
+        public DataTable DisplayJsonData()
+        {
+            StreamReader sr = new StreamReader(Server.MapPath("~/Products.json"));
+            string json = sr.ReadToEnd();
+            sr.Close();
+            var table = JsonConvert.DeserializeObject<DataTable>(json);
+            return table;
+        }
+
+        protected void btnSaveToFile_Click(object sender, EventArgs e)
+        {
+            System.IO.StreamWriter myFile = new System.IO.StreamWriter(Server.MapPath("Products.json"));
+            if (!txtMain.Text.Equals(" "))
+            {
+                myFile.WriteLine(txtMain.Text);
+            }
+            myFile.Close();
+        }
+    }
+}
